379f896fdef39009a85a6232b693c433f3370678
IbraBach <ibraalberola@gmail.com>
2021-07-10 10:02:33 +0200

    Update myApp.js
    Mongoose conection added.

--- myApp.js
+++ myApp.js
@@ -1,7 +1,19 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+const mongoDB = require('mongodb');
+const { Schema } = mongoose;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, 'useCreateIndex': true});
 
 
-let Person;
+ const personSchema  = new Schema({
+   name: { type: String, required: true },
+   age: { type: Number, required: true },
+   favoriteFood: { type: [String], index: true }
+ });
+
+let Person = mongoose.model('Person', personSchema);
+
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
